79e610e48a340a043d9c5fab630de8e348d6dbab
Silas <silasnas2018@gmail.com>
2023-09-11 21:41:33 -0300

    ap03(selecao): exemplo if/else finalizado

---
+++ ExemploIfElse.java
@@ -0,0 +1,20 @@
+
+
+public class ExemploIfElse {
+    public static void main (String [] args){
+        //nome completamente qualificado da classe 
+        //fully qualifide name
+        java.util.Scanner leitor = new java.util.Scanner(System.in);
+        double nota;
+
+        System.out.println("Digite a nota : ");
+        nota = leitor.nextDouble();
+
+        if(nota >= 70)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+
+    }
+    
+}
