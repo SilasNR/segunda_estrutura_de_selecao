e792aba7d14e5e0fcba8c2eac84b70ce2f9706fc
Silas <silasnas2018@gmail.com>
2023-09-11 21:07:36 -0300

    ap03(selecao): exemplo if finalizado

--- ExemploIf.java
+++ ExemploIf.java
@@ -3,12 +3,13 @@ import javax.swing.JOptionPane;
 public class ExemploIf{
     static public void main(String [] args){
         //declaração de variáveis
-        
-
         //entrada
+        double nota = Double.parseDouble
+        (JOptionPane.showInputDialog(null,"Digite a nota"));
 
         //processamento
-
         //saida
+        if(nota >= 70)
+            JOptionPane.showMessageDialog(null,"Aprovado");
     }
 }
